21bedca61aa6e2c56d07821389f1ecdcb8c8e48b
Wesley Willians <wesleywillians@gmail.com>
2020-06-26 19:47:32 -0400

    wesley show

--- main.go
+++ main.go
@@ -14,5 +14,5 @@ func main() {
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello Full Cycle Developers!!!!</h1>")
+	fmt.Fprintf(w, "<h1>Hello Full Cycle Developers!!!!</h1><h2>Wesley Show</h2>")
 }
